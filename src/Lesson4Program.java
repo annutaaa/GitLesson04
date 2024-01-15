ac36b7b506977ccc321ee95f93c18f41647e3970
annutaaa <akramnicna@gmail.com>
2024-01-15 18:58:52 +0100

    add new system out println

--- src/Lesson4Program.java
+++ src/Lesson4Program.java
@@ -1,6 +1,7 @@
 public class Lesson4Program {
     public static void main(String[] args) {
 System.out.println("Hello would from Ann to Git");
+System.out.println("Second commit to Git");
     }
 
 
